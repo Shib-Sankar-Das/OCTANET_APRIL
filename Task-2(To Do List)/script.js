ad36af8d2ed0182b1e76e17fa794431bfb45d8d0
Shib Sankar Das <shibsankardas721@gmail.com>
2024-04-13 13:23:44 +0530

    To Do List - List making

--- Task-2(To Do List)/script.js
+++ Task-2(To Do List)/script.js
@@ -7,6 +7,14 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+var maindiv = document.querySelector(".content");
+
+var submit = document.getElementById("submit");
+
+var gifElement = document.getElementById('gif');
+
+var page = document.getElementById("page");
+
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
@@ -23,3 +31,24 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+submit.onclick = function() {
+  /*var task = document.getElementById("task").value;
+  var div = document.createElement("div");
+  div.className = "task";
+  div.innerHTML = task;
+  maindiv.appendChild(div);*/
+  modal.style.display = "none";
+  maindiv.style.animation = 'fadeOut 3s';
+  maindiv.addEventListener('animationend', function() {
+    // Set display to 'none' after animation ends
+    maindiv.style.display = 'none';
+    page.style.justifyContent = 'center';
+    gifElement.style.display = 'flex';
+    gifElement.style.animation = 'fadeIn 3s';
+  });
+  /*maindiv.style.display = 'none';*/
+  // Wait for the page to fully load before executing JavaScript
+  // Display the GIF
+}
+
